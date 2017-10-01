6f27175a0d98badcabf893bf0c748f93146f1e3f
thomasaiman <32423251+thomasaiman@users.noreply.github.com>
2017-10-01 14:26:32 -0500

    fixed getNasaPhoto()

--- mars.js
+++ mars.js
@@ -7,12 +7,12 @@ twilio.accountSid = 'AC90de1b430b0883796e652f70646032b3';
 twilio.apiKey = '17d80c21fc2cb4ee8741dfb9714024d6';
 
 // the image on the right
-const image = 'FIX ME'; // we need to get the image
+const image = document.getElementById('mars'); // we need to get the image
 
 function getNasaPhoto() {
     // we're getting the user input from the document
     let date = document.getElementById('date').value;
-    let camera = "FIX ME"; // we need to get the camera value
+    let camera = document.getElementById('camera').value; // we need to get the camera value
 
     // we're putting the information into a nice format
     let params = {
